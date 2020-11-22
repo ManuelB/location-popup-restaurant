8fc651ac039e21a914806a34815ebf9314fb3cde
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2020-11-22 21:22:14 +0100

    Fixed problem when object does not contain a polygon

--- app.js
+++ app.js
@@ -55,7 +55,8 @@ const getParkingSpaceInfo = (evt, oFeature) => {
   let areaParkingSpot = 0;
   if (evt) {
     // if this is a polygon calculate the area
-    if("map" in evt.object.geometry.coordinates[0]) {
+    if(evt && evt.object && evt.object.geometry && evt.object.geometry.coordinates
+      && evt.object.geometry.coordinates.length > 0 && "map" in evt.object.geometry.coordinates[0]) {
       let coord = evt.object.geometry.coordinates[0].map(e => new jsts.geom.Coordinate(e[0], e[1]));
       areaParkingSpot = factory.createPolygon(coord).getArea();
     }
